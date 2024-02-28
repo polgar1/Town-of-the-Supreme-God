5c7e4b053e3d2483e89cc9c68a67d8a863632cd8
Punga911 <punga9111@gmail.com>
2024-02-28 13:39:30 +0200

    Player Animations

--- Player.gd
+++ Player.gd
@@ -2,6 +2,9 @@ extends CharacterBody2D
 
 @onready var anim = $AnimationPlayer
 var speed : float = 15
+@onready var eyes: Sprite2D = $Sprite2D/Eyes
+@onready var sprite = $Sprite2D
+
 func _physics_process(delta):
 	var motion = Vector2()
 	
@@ -22,12 +25,23 @@ func _process(delta):
 	handleAnim()
 
 func handleAnim():
-	if(Input.is_action_just_pressed("Down") and (Input.is_action_just_pressed("Left") or Input.is_action_just_pressed("Right"))) or (Input.is_action_just_pressed("Up") and (Input.is_action_just_pressed("Right") or Input.is_action_just_pressed("Left"))):
+	if(Input.is_action_pressed("Down") and (Input.is_action_pressed("Left") or Input.is_action_pressed("Right"))) or (Input.is_action_pressed("Up") and (Input.is_action_pressed("Right") or Input.is_action_pressed("Left"))):
 		anim.play("walkVert")
 	elif Input.is_action_pressed("Down") or Input.is_action_pressed("Up"):
 		anim.play("walkVert")
 	elif Input.is_action_pressed("Right") or Input.is_action_pressed("Left"):
 		anim.play("walkHor")
-		
 	else:
 		anim.play("idle")
+	
+	if anim.current_animation == "idle" or (anim.current_animation == "walkVert" and Input.is_action_pressed("Down")):
+		eyes.visible = true
+	else:
+		eyes.visible = false
+	
+	if Input.is_action_pressed("Left"):
+		sprite.scale.x = 1
+		eyes.scale.x = -1
+	if Input.is_action_pressed("Right"):
+		sprite.scale.x = -1
+		eyes.scale.x = 1
